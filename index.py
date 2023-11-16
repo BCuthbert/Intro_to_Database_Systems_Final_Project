c4dc0b7d1c36624ec7935261a6fb88e58d2432fc
2Beast101 <bradendavidc@yahoo.com>
2023-11-16 18:45:28 -0500

    Added table.sql.

--- index.py
+++ index.py
@@ -1,18 +1,54 @@
 #! /usr/bin/python3
-
-from flask import Flask, jsonify
-from flask_cors import CORS
-from flask import request
-import json
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
 
 app = Flask(__name__)
-CORS(app)
 
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'bcuthbe1'
+app.config['MYSQL_PASSWORD'] = 'x'
+app.config['MYSQL_DB'] = 'bcuthbe1'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def homepage():
     print("Hello world")
+
+@app.route('/login')
+def login():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return redirect(url_for('home'))
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('index.html', msg=msg)
+
+    # http://localhost:5000/python/logout - this will be the logout page
+
 
 
 if __name__ == "__main__":
