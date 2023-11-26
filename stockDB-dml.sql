3539933133ff646ea7b7242207dd7680f4c18faf
Brent Kent <bwarrin3@kent.edu>
2023-11-26 13:50:49 -0500

    Complete price_history values

--- stockDB-dml.sql
+++ stockDB-dml.sql
@@ -14,32 +14,32 @@ insert into stocks values ('NVDA','Nvidia');
 insert into stocks values ('AMD','Advanced Micro Devices');
 
 --AAPL
-insert into price_history values ('2023-11-09','AAPL', ...);
-insert into price_history values ('2023-11-10','AAPL', ...);
-insert into price_history values ('2023-11-13','AAPL', ...);
-insert into price_history values ('2023-11-14','AAPL', ...);
-insert into price_history values ('2023-11-15','AAPL', ...);
+insert into price_history values ('2023-11-13','AAPL', 184.80);
+insert into price_history values ('2023-11-14','AAPL', 187.44);
+insert into price_history values ('2023-11-15','AAPL', 188.01);
+insert into price_history values ('2023-11-16','AAPL', 189.71);
+insert into price_history values ('2023-11-17','AAPL', 189.69);
 
 --AMZN
-insert into price_history values ('2023-11-09','AMZN', ...);
-insert into price_history values ('2023-11-10','AMZN', ...);
-insert into price_history values ('2023-11-13','AMZN', ...);
-insert into price_history values ('2023-11-14','AMZN', ...);
-insert into price_history values ('2023-11-15','AMZN', ...);
+insert into price_history values ('2023-11-13','AMZN', 142.59);
+insert into price_history values ('2023-11-14','AMZN', 145.80);
+insert into price_history values ('2023-11-15','AMZN', 143.20);
+insert into price_history values ('2023-11-16','AMZN', 142.83);
+insert into price_history values ('2023-11-17','AMZN', 145.18);
 
 --NVDA
-insert into price_history values ('2023-11-09','NVDA', ...);
-insert into price_history values ('2023-11-10','NVDA', ...);
-insert into price_history values ('2023-11-13','NVDA', ...);
-insert into price_history values ('2023-11-14','NVDA', ...);
-insert into price_history values ('2023-11-15','NVDA', ...);
+insert into price_history values ('2023-11-13','NVDA', 486.20);
+insert into price_history values ('2023-11-14','NVDA', 496.56);
+insert into price_history values ('2023-11-15','NVDA', 488.88);
+insert into price_history values ('2023-11-16','NVDA', 494.80);
+insert into price_history values ('2023-11-17','NVDA', 492.98);
 
 --AMD
-insert into price_history values ('2023-11-09','AMD', ...);
-insert into price_history values ('2023-11-10','AMD', ...);
-insert into price_history values ('2023-11-13','AMD', ...);
-insert into price_history values ('2023-11-14','AMD', ...);
-insert into price_history values ('2023-11-15','AMD', ...);
+insert into price_history values ('2023-11-13','AMD', 116.79);
+insert into price_history values ('2023-11-14','AMD', 119.88);
+insert into price_history values ('2023-11-15','AMD', 118.00);
+insert into price_history values ('2023-11-16','AMD', 119.83);
+insert into price_history values ('2023-11-17','AMD', 120.62);
 
 --jbohnam
 insert into lots values ();
