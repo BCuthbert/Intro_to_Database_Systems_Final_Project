330106b626f28e883ba58a23d510d7236af5306d
dakotaSeeker <dsicher@kent.edu>
2023-12-01 12:34:31 -0500

    dml working, changed lot_id to auto_incr

--- stockDB-ddl.sql
+++ stockDB-ddl.sql
@@ -1,33 +1,29 @@
 
 create table account
-	(id AUTO_INCREMENT varchar(5),
+	(id                    int AUTO_INCREMENT,
      user           	   varchar(20),
      password       	   varchar(255),
      creation_date  	   date,
      cash           	   numeric(10,2),
-     accnt_value as (SELECT sum(lot_value) FROM lots WHERE id = lots.id),
      primary key (id)
 	);
-
-create table lots(
-     id                 varchar(5),
-     lot_num            varchar(5),
-     ticker             varchar(4),
-     num_shares         numeric(6,0),
-     purchase_price     numeric(6,2),
-     purchase_date      date,
-     lot_value as (num_shares * (SELECT price FROM stocks WHERE ticker == stocks.ticker)),
-     primary key (id,lot_num),
-     foreign key (id) references account (id),
-     foreign key (ticker) references stocks (ticker),
-    );
-
 
 create table stocks(
      ticker             varchar(5),
      company_name       varchar(30),
-     price as (SELECT from price_history WHERE ticker == price_history.ticker AND price_history.price_date == CURDATE()),
      primary key (ticker)
+    );
+
+create table lots(
+     lot_num            int AUTO_INCREMENT,
+     id                 int,
+     ticker             varchar(4),
+     num_shares         numeric(6,0),
+     purchase_price     numeric(6,2),
+     purchase_date      date,
+     primary key (lot_num,id),
+     foreign key (id) references account (id),
+     foreign key (ticker) references stocks (ticker)
     );
 
 create table price_history(
